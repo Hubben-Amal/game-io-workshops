7f754f557ef522d1f5183c33767ae6f9e344a5e2
Theo Dedeken <theo.dedeken@telenet.be>
2020-02-18 11:25:18 +0100

    more status updates

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,13 +3,15 @@
 set -e
 
 cd $GITHUB_WORKSPACE/_site
+echo 'navigate into dir'
 remote_repo="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" && \
-remote_branch="gh-pages" && \
+remote_branch="gh-pages"
+echo 'Add remote repo'
 git init && \
 git config user.name "${GITHUB_ACTOR}" && \
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
 git add . && \
-echo -n 'Files to Commit:' && ls -l | wc -l && \
+echo -n 'Files to Commit:' && ls -l | wc -l
 git commit -m'action build' > /dev/null 2>&1 && \
 git push --force $remote_repo master:$remote_branch > /dev/null 2>&1 && \
 rm -fr .git && \
