eadaec3d0e229fad8693df1b11c5de21d48b0290
Theo Dedeken <theo.dedeken@telenet.be>
2020-02-18 11:30:43 +0100

    sudo privileges

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,13 +3,15 @@
 set -e
 
 cd $GITHUB_WORKSPACE/_site
-echo 'navigate into dir'
+echo 'Navigate into dir'
 remote_repo="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" && \
 remote_branch="gh-pages"
 echo 'Add remote repo'
-git init && \
+git init
+echo 'Git init'
 git config user.name "${GITHUB_ACTOR}" && \
-git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+echo 'Set user details'
 git add . && \
 echo -n 'Files to Commit:' && ls -l | wc -l
 git commit -m'action build' > /dev/null 2>&1 && \
